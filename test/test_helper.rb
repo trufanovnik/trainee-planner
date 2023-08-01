0ab2629a3d2cf65ff65aeeaf043e8016fbd5300c
Trufanov Nikita <trufanovwork@gmail.com>
2023-08-01 12:53:49 +0300

    add settings shoulda-matchers test/test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,10 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
