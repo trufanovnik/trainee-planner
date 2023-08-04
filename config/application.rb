71ad4274d09510871b5b73cf35ab4b1f3581728a
Trufanov Nikita <trufanovwork@gmail.com>
2023-08-04 12:31:16 +0300

    create rubocop.yml and configurate it

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +13,11 @@ module TraineePlanner
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Added linter auto-correction for generated files
+    config.generators.after_generate do |files|
+      parsable_files = files.filter { |file| file.end_with?('.rb') }
+      system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
