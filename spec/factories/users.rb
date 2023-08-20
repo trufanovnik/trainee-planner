6d8b8291974bc1737d863d7aa9d6c12822977728
Trufanov Nikita <trufanovwork@gmail.com>
2023-08-20 22:31:31 +0300

    need fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,8 @@
 
 FactoryBot.define do
   factory :user do
+    first_name { Faker::Name.first_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create' do
+    let(:user) { FactoryBot.build(:user) }
+    subject { user }
+    it { should respond_to(:first_name) }
+    it { should respond_to(:email) }
+  end
 end
